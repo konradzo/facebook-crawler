d589a34595dbaa2bb5029ff4eddb1e0a43527b8a
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2021-01-11 21:55:51 +0100

    initiall commit

---
+++ src/test/java/pl/kzochowski/facebookcrawler/FacebookCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.facebookcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
